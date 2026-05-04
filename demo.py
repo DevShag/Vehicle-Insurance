32260be92d883f048d8a5e441f501397eb3463c6
DevShag <alishaguftadev@gmail.com>
2026-05-04 11:25:38 +0530

    data ingestion pipeline completed

---
+++ demo.py
@@ -0,0 +1,14 @@
+from src.logger import logging
+from src.pipeline.training_pipeline import TrainPipeline
+
+"""
+logging.debug("This is a debug message")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
+"""
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
+
